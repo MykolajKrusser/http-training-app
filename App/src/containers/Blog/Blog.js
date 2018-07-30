ace7e8e32d4410b58786b6e2698af4cb4afa4962
Nicolas <nicolaskrusser@gmail.com>
2018-07-30 18:04:03 +0200

    get data from REST API with axios

--- App/src/containers/Blog/Blog.js
+++ App/src/containers/Blog/Blog.js
@@ -3,16 +3,31 @@ import React, { Component } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        post: []
+    }
+
+    componentDidMount(){
+        axios.get('http://jsonplaceholder.typicode.com/posts').then(respons => {
+            this.setState({post: respons.data})
+        })
+    }
+
     render () {
+        const posts = this.state.post.map(post =>{
+            return <Post 
+                key={post.id}
+                title={post.title}
+                />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
