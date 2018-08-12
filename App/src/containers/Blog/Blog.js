43e8715c701342e59dcbf5bb2567bd2d8740b26a
Nicolas <nicolaskrusser@gmail.com>
2018-08-12 14:49:36 +0200

    Using LINKS to switch components

--- App/src/containers/Blog/Blog.js
+++ App/src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 
 class Blog extends Component {
     
@@ -11,8 +11,12 @@ class Blog extends Component {
             <div>
                 <nav className="Nav">
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/new-post">New Post</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to={{
+                            pathname: '/new-post',
+                            hash: '#submit',
+                            search: '?quick-submit=true'
+                        }}>New Post</Link></li>
                     </ul>
                 </nav>
                 {/*<Route path="/" exact render={()=><h1>Home</h1>}/>
