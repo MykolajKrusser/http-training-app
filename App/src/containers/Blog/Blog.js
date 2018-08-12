8531a2b160323e44f882aae48feed9225b081720
Nicolas <nicolaskrusser@gmail.com>
2018-08-12 14:33:35 +0200

    switching between components

--- App/src/containers/Blog/Blog.js
+++ App/src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import {Route} from 'react-router-dom';
 
@@ -17,6 +18,7 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={()=><h1>Home</h1>}/>
                 <Route path="/"  render={()=><h1>Home22</h1>}/>*/}
                 <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
